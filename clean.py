de5c90ce8af402853dfab6c158c4862645eb5f3d
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2020-09-18 13:41:56 +0300

    Add direction column, clean rows that have null or empty values or unwanted train lines

--- clean.py
+++ clean.py
@@ -6,9 +6,9 @@ import datetime
 # Location of data directory
 data_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
 json_files = glob.glob(os.path.join(data_folder, "raw", "*.json"))
-extra_columns = ["actualTime", "cancelled", "commercialTrack", "differenceInMinutes", 
-				 "scheduledTime", "stationShortCode", "trainStopping", "categoryCodeId", "categoryCode", 
-				 "detailedCategoryCode", "detailedCategoryCodeId", "year", "month", "day", "hour"]
+extra_columns = ["actualTime", "cancelled", "commercialTrack", "differenceInMinutes",
+				 "scheduledTime", "stationShortCode", "trainStopping", "categoryCodeId", "categoryCode",
+				 "detailedCategoryCode", "detailedCategoryCodeId", "year", "month", "day", "hour", "direction"]
 
 dfs = []
 for json_file in json_files:
@@ -17,8 +17,9 @@ for json_file in json_files:
 	df = df[["trainCategory", "commuterLineID", "timeTableRows"]]
 	df = pd.concat([df, pd.DataFrame(index=df.index, columns=extra_columns)], axis=1)
 
-# Set row values from timeTableRows to created extra columns 
+# Set row values from timeTableRows to created extra columns
 	for i, row in df.iterrows():
+		direction = 0 if row['timeTableRows'][-1]["stationShortCode"] == "HKI" else 1
 		for k in row["timeTableRows"]:
 			[k.pop(key, None) for key in["stationUICCode", "countryCode", "type", "commercialStop", "liveEstimateTime", "estimateSource", "unknownDelay", "trainReady"]]
 
@@ -27,10 +28,10 @@ for json_file in json_files:
 			row["day"] = date.day
 			row["hour"] = date.hour
 			row["year"] = date.year
+			row["direction"] = direction
 
-			if len(k["causes"]) > 0:
-				[k["causes"][0].pop(key, None) for key in ["thirdCategoryId", "thirdCategoryCodeId"]]
 			if k["causes"]:
+				[k["causes"][0].pop(key, None) for key in ["thirdCategoryId", "thirdCategoryCodeId"]]
 				for key in k["causes"][0]:
 					row[key] = k["causes"][0][key]
 			k.pop("causes", None)
@@ -40,5 +41,10 @@ for json_file in json_files:
 	dfs.append(df)
 
 df = pd.concat(dfs)
-destination = os.path.join(data_folder, "clean", "trains.csv")
-df.to_csv(destination, index=False)
+
+# clean up NaNs from certain columns, also remove N and V trains and rows with empty commercialTrack
+filtered_cols = ["cancelled", "commercialTrack", "commuterLineID"]
+df = df[(df[filtered_cols].notnull().all(1)) & (df["commuterLineID"] != "N") & (df["commuterLineID"] != "V") &  (df["commercialTrack"] != "")]
+
+destination=os.path.join(data_folder, "clean", "trains.csv")
+df.to_csv(destination, index = False)
