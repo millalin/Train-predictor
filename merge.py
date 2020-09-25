f0d283481afe643044a53e29164364094d05aefa
Milla <47038050+millalin@users.noreply.github.com>
2020-09-25 18:03:56 +0300

    adding cvs files with diffrent weather area data, merging weather dat… (#8)
    * adding cvs files with diffrent weather area data, merging weather data to train data according to relevant area
    
    * changing trains data to be loaded from right folder

--- merge.py
+++ merge.py
@@ -1,14 +1,31 @@
 import glob
 import pandas as pd
 
-trains_data = pd.read_csv("trains.csv", dtype={"commercialTrack": str}) 
-path =r'data_weather'
-filenames = glob.glob(path + "/*.csv")
-dfs = []
-for filename in filenames:
-    dfs.append(pd.read_csv(filename))
+trains_data = pd.read_csv("clean/trains.csv", dtype={"commercialTrack": str}) 
 
-weather_data = pd.concat(dfs, ignore_index=True)
+path =r'data_weather'
+
+def get_df(fn, area):
+    filenames = glob.glob(path + fn)
+    dfs = []
+    for filename in filenames:
+        dfs.append(pd.read_csv(filename))
+
+    data = pd.concat(dfs, ignore_index=True)
+    data['weather_area'] = area
+    return data
+
+data_helsinkivantaa = get_df("/hki-vantaa/*.csv", 1)
+data_espoo = get_df("/espoo/*.csv", 2)
+data_kirkkonummi = get_df("/kirkkonummi/*csv", 3)
+data_lahti = get_df("/lahti/*csv", 4)
+data_hameenlinna = get_df("/hameenlinna/*csv", 5)
+data_tampere = get_df("/tampere/*csv", 6)
+data_hyvinkaa = get_df("/hyvinkaa/*csv", 7)
+data_lahti = get_df("/lahti/*csv", 8)
+
+frames = [data_helsinkivantaa, data_espoo, data_kirkkonummi, data_lahti, data_hameenlinna, data_tampere, data_hyvinkaa, data_lahti]
+weather_data = pd.concat(frames)
 
 weather_data = weather_data.rename(columns = {'Vuosi': 'year', 'Kk': 'month', 'Pv': 'day', 'Klo': 'hour',
 'Aikavyöhyke':'timezone', 'Sateen intensiteetti (mm/h)':'rain', 'Lumensyvyys (cm)':'snowDepth', 'Ilman lämpötila (degC)':'celcius',
@@ -16,6 +33,7 @@ weather_data = weather_data.rename(columns = {'Vuosi': 'year', 'Kk': 'month', 'P
 
 weather_data["hour"] = weather_data["hour"].astype(str).str.replace(":00","").astype(int)
 
-df = pd.merge(trains_data, weather_data, on= ['year', 'month', 'day', 'hour'], how='left')
+df = pd.merge(trains_data, weather_data, on= ['year', 'month', 'day', 'hour', 'weather_area'], how='left')
 
 df.to_csv(r'trains_and_weather.csv', index = False)
+
