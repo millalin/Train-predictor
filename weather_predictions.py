07442b4f43a07e1d6c164778d80f45143b90d595
Heli Huhtilainen <heli.huhtilainen@helsinki.fi>
2020-09-27 16:40:46 +0300

    Make queries of latest observations for five places, save result to csv

--- weather_predictions.py
+++ weather_predictions.py
@@ -5,14 +5,7 @@ import datetime
 import json
 import pandas as pd
 
-# dir_path = os.path.dirname(os.path.realpath(__file__))
-# base_url = "http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::multipointcoverage&place=helsinki&"
-# destination_folder = os.path.join(dir_path, "we_pred", "raw")
-
-# r = requests.get(base_url, destination_folder)
-
-# Retrieve the latest hour of data from a bounding box
-
+# Retrieve the latest observations for several locations, save to csv-file
 
 end_time = datetime.datetime.utcnow()
 start_time = end_time - datetime.timedelta(hours=5)
@@ -21,30 +14,39 @@ start_time = start_time.isoformat(timespec="seconds") + "Z"
 # -> 2020-07-07T12:00:00Z
 end_time = end_time.isoformat(timespec="seconds") + "Z"
 # -> 2020-07-07T13:00:00Z
-hki_vantaa_lat_lon = 60.3267, 24.95675
+# 'Helsinki-Vantaa airport': {'fmisid': 16000063, 'latitude': 60.3267, 'longitude': 24.95675} # cuurently not used in the query
 
+places = ['Kumpula,Helsinki', 'Tapiola,Espoo',
+          'Kirkkonummi', 'Lahti', 'Tampere']
+place_index = ['Helsinki', 'Espoo', 'Kirkkonummi', 'Lahti', 'Tampere']
+# Hämeenlinna and Hyvinkää queries not implemented
+# places = ['Kumpula,Helsinki', 'Espoo', 'Kirkkonummi', 'Lahti', 'Hämeenlinna, 'Tampere', 'Hyvinkää', 'Lahti']
+rows = []
+for place in places:
+    obs = download_stored_query("fmi::observations::weather::multipointcoverage",
+                                args=["place=" + place,
+                                      "starttime=" + start_time, "endtime=" + end_time])
+    # print(obs.location_metadata)
+    # print(obs.data)
+    time_of_day = max(obs.data.keys())
+    print('timestamp', time_of_day)
 
-# 'Helsinki': {'fmisid': 658225, 'latitude': 60.16952, 'longitude': 24.93545}
-# 'Helsinki-Vantaa airport': {'fmisid': 16000063, 'latitude': 60.3267, 'longitude': 24.95675},
+    weather_station = list(obs.data[time_of_day].keys())[0]
+    print(weather_station)
 
-# obs_hki_vantaa = download_stored_query("fmi::forecast::hirlam::surface::point::multipointcoverage",
-#                                        args=["starttime=" + start_time,
-#                                              "endtime=" + end_time, "latlon=60.3267,24.95675"])
+    data = obs.data[time_of_day][weather_station]
+    rain = data['Precipitation intensity']['value']
+    snowDepth = data['Snow depth']['value']
+    celcius = data['Air temperature']['value']
+    visibility = data['Horizontal visibility']['value']
+    windGustSpeed = data['Gust speed']['value']
+    windSpeed = data['Wind speed']['value']
 
-# obs_hki_vantaa = download_stored_query("fmi::observations::weather::multipointcoverage",
-#                                        args=["place=Helsinki",
-#                                              "starttime=" + start_time, "endtime=" + end_time])
-place = 'Helsinki'
-obs_hki = download_stored_query("fmi::observations::weather::multipointcoverage",
-                                       args=["place=" + place,
-                                             "starttime=" + start_time, "endtime=" + end_time])                                             
+    row = [time_of_day.year, time_of_day.month, time_of_day.day, time_of_day.hour,
+           'UTC', rain, snowDepth, celcius, visibility, windGustSpeed, windSpeed]
+    rows.append(row)
 
-print(obs_hki.location_metadata)
-print(obs_hki.data)
-time_of_day = list(obs_hki.data.keys())[0]
-rain = obs_hki.data[time_of_day][place]['Precipitation intensity']['value']
-
-df = pd.DataFrame(columns=['year', 'month',  'day',  'hour', 'timezone',
-                           'rain', 'snowDepth', 'celcius', 'visibility', 'windGustSpeed', 'windSpeed'])
+df = pd.DataFrame(rows, columns=['year', 'month',  'day',  'hour', 'timezone',
+                                 'rain', 'snowDepth', 'celcius', 'visibility', 'windGustSpeed', 'windSpeed'], index=place_index)
 print(df)
-#df.loc[[0], ['year', 'month',  'day',  'hour', 'timezone', 'rain', 'snowDepth', 'celcius', 'visibility', 'windGustSpeed', 'windSpeed']] = time_of_day.year, time_of_day.month, time_of_day.day, time_of_day.hour, 'UTC', rain,
+df.to_csv(r'data-we-pred/latest_weather_observations.csv', index=True)
