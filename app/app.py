0d37fe8dd00263613b5675d2e5c944d189cbdfa3
Milla <47038050+millalin@users.noreply.github.com>
2020-10-07 16:49:22 +0300

    Dropdown (#23)
    * dropdown to stations
    
    * adding commuterLinesID json, adding dropdowns to train and station

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ import pickle
 import joblib
 import helpers.weather_for_model as weather
 import pandas as pd
+import json
 
 app = Flask(__name__)
 model = joblib.load('model.z')
@@ -17,25 +18,34 @@ def create_stations_json():
 
 stations = create_stations_json()
 
+with open('../utils/lines.json', 'r') as f:
+    lines = json.load(f)
 
 @app.route('/')
 def home():
-    return render_template('index.html', stations=stations)
+    return render_template('index.html', stations=stations, lines=lines)
 
 @app.route('/predict',methods=['POST'])
 def predict():
 
     inputs = [int(x) for x in request.form.values()]
+    select_station = request.form.get('station')
+    select_station = int(select_station)
+    select_line = request.form.get('lineID')
+    select_line = int(select_line)
+
     print(inputs)
     weatherPrediction = weather.give_prediction(inputs[2], inputs[3], inputs[4])
     print(weatherPrediction)  # 'rain', 'celcius', 'windGustSpeed', 'windSpeed'
     inputs.extend(weatherPrediction)
+    inputs = [select_line] + inputs[1:1] + [select_station] + inputs[2:]  
     features = [np.array(inputs)]
+    print("feat ", features)
     prediction = model.predict(features)
-
     res = int(prediction[0])
 
-    return render_template('index.html', prediction_minutes='Delay {} minute(s)'.format(res), stations=stations)
+    return render_template('index.html', prediction_minutes='Delay {} minute(s)'.format(res), stations=stations, lines=lines)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
